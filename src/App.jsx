8fac20264ac1fb8a2a7b4e123c1afb402432088b
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-02-03 21:49:41 -0600

    Add WeatherApp component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import WeatherApp from './Components/WeatherApp/WeatherApp';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <WeatherApp/>
     </div>
   );
 };
